8688509d59266babc778d40c662b634705baa444
Vojtech Burian <vojtechburian@users.noreply.github.com>
2026-06-09 18:18:01 +0200

    Configure GitHub Pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const repo = "/sipky_web";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: repo,
+  assetPrefix: repo,
+  trailingSlash: true,
+};
 
 export default nextConfig;
